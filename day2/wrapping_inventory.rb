1838e396e3f5ae4d0829767a07056ed64acf3123
Alex Wayne <alex@beautifulpixel.com>
2015-12-04 12:14:06 -0800

    Day 2 part 2!

--- day2/wrapping_inventory.rb
+++ day2/wrapping_inventory.rb
@@ -1,16 +1,27 @@
-module WrappingInventory
-  def self.required_paper_for_package_list(list)
-    packages = list.split("\n")
+class WrappingInventory
 
-    packages.reduce(0) do |total, package|
-      dimensions = package.split('x').map(&:to_i)
-      total + required_paper_for_package(dimensions[0], dimensions[1], dimensions[2])
+  # Create a new WrappingInventory from a text list of package dimensions.
+  def initialize(package_list)
+    @packages = process_input package_list
+  end
+
+  # Returns the amount of paper required to wrap all presents.
+  def required_paper
+    sum do |dimensions|
+      required_paper_for_package dimensions
     end
   end
 
-  def self.required_paper_for_package(w, l, h)
+  # Returns the amount of paper required to wrap one present of [w, l, h] dimensions.
+  def required_paper_for_package(dimensions = [])
+    w, l, h = dimensions
+
     # Get the area of each side twice to cover the whole box.
-    sides = [w * l, w * h, l * h] * 2
+    sides = [
+      w * l,
+      w * h,
+      l * h
+    ] * 2
 
     # Add again the smallest side.
     sides << sides.min
@@ -18,10 +29,46 @@ module WrappingInventory
     # Sum the area of each segment.
     sides.reduce :+
   end
+
+  # Returns the amount of ribbon required to wrap all presents.
+  def required_ribbon
+    sum do |dimensions|
+      required_ribbon_for_package dimensions
+    end
+  end
+
+  # Returns the amount of ribbon required to wrap one present of [w, l, h] dimensions.
+  def required_ribbon_for_package(dimensions)
+    w, l = dimensions
+
+    # Calucalate the smallest perimiter from the two shortest dimensions.
+    ribbon = (w + l) * 2
+
+    # Add in the volume of the package.
+    ribbon += dimensions.reduce(:*)
+
+    # Return the result!
+    ribbon
+  end
+
+  private
+    def process_input(package_list)
+      package_list.split("\n").map do |line|
+        line.split('x').map(&:to_i).sort
+      end
+    end
+
+    def sum
+      @packages.reduce(0) do |total, dimensions|
+        total + yield(dimensions)
+      end
+    end
 end
 
 # When this file is run directly, do the execerise!
 if __FILE__ == $0
   input = File.read File.expand_path('../input.txt', __FILE__)
-  puts "We need #{ WrappingInventory.required_paper_for_package_list input } square feet of wrapping paper"
+  inv = WrappingInventory.new input
+  puts "We need #{ inv.required_paper } square feet of wrapping paper"
+  puts "We need #{ inv.required_ribbon } feet of ribbon"
 end
--- day2/wrapping_inventory_spec.rb
+++ day2/wrapping_inventory_spec.rb
@@ -4,20 +4,41 @@ include RSpec
 require_relative './wrapping_inventory'
 
 describe WrappingInventory do
-  describe '.required_paper_for_package_list' do
-    it 'returns the total amount of wrapping paper needed for a text list of package dimensions' do
+  describe '.required_paper' do
+    it 'returns the total amount of wrapping paper needed for all presents' do
       list = <<-PACKAGELIST
         2x3x4
         1x2x3
       PACKAGELIST
 
-      expect(WrappingInventory.required_paper_for_package_list list).to equal(82)
+      inv = WrappingInventory.new list
+      expect(inv.required_paper).to equal(82)
     end
   end
 
   describe '.required_paper_for_package' do
     it "returns the square feet of paper required to wrap a present" do
-      expect(WrappingInventory.required_paper_for_package 2,3,4).to equal(58)
+      inv = WrappingInventory.new ''
+      expect(inv.required_paper_for_package [2, 3, 4]).to equal(58)
+    end
+  end
+
+  describe '.required_ribbon' do
+    it 'returns the total amount of ribbon needed for all presents' do
+      list = <<-PACKAGELIST
+        2x3x4
+        1x1x10
+      PACKAGELIST
+
+      inv = WrappingInventory.new list
+      expect(inv.required_ribbon).to equal(48)
+    end
+  end
+
+  describe '.required_ribbon_for_package' do
+    it "returns the length of ribbon required to wrap a present" do
+      inv = WrappingInventory.new ''
+      expect(inv.required_ribbon_for_package [2, 3, 4]).to equal(34)
     end
   end
 end
