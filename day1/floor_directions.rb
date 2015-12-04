ff5608ffd4d22670b7f298e95426b92d805e23bc
Alex Wayne <alex@beautifulpixel.com>
2015-12-04 11:10:15 -0800

    Comments don't suck.

--- day1/floor_directions.rb
+++ day1/floor_directions.rb
@@ -1,12 +1,18 @@
 class FloorDirections
+
+  # Accepts a string of "(" and ")" characters, where "(" means
+  # to go up a floor, and ")" means to go down.
   def initialize(directions)
     @directions = process_input directions
   end
 
+  # Total the ups and down to find the destination floor.
   def find_floor
     @directions.reduce :+
   end
 
+  # Find the step in the directions that causes you to enter
+  # the basement.
   def enter_basement
     floor = 0
     @directions.each_with_index do |value, i|
@@ -17,13 +23,17 @@ class FloorDirections
   end
 
   private
-  def process_input(directions)
-    directions.split('').map do |char|
-      char == '(' ? 1 : -1
+
+    # Convert a string of ( and ) to an array of +1 and -1 integers.
+    # This makes them far easier to enumerate and work with.
+    def process_input(directions)
+      directions.split('').map do |char|
+        char == '(' ? 1 : -1
+      end
     end
-  end
 end
 
+# When this file is run directly, do the execerise!
 if __FILE__ == $0
   input = File.read File.expand_path('../input.txt', __FILE__)
   directions = FloorDirections.new(input)
